5fb8ae2cd6a57121e769dc6564b68b02bfb07922
xdream oldlu <xdream@gmail.com>
2023-12-26 00:25:43 +0800

    done save, need load...

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 import streamlit as st
-import requests
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 
 from qdrant_client import QdrantClient
 
-# from qdrant_client.http.models import VectorParams, Distance
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.qdrant import Qdrant
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
+from utils import get_pdf_text, get_text_chunks
 
 load_dotenv()
 
@@ -23,79 +20,36 @@ embeddings = GoogleGenerativeAIEmbeddings(
 )
 
 db_client = QdrantClient(
-    url="192.168.50.16:6333",
+    url=os.getenv("QDRANT_URL"),
     # api_key=os.getenv('QDRANT_API_KEY')
 )
-doc_store = Qdrant(
-    client=db_client,
-    collection_name=os.getenv("QDRANT_COLLECTION_NAME") or "test",
-    embeddings=embeddings,
-)
-
-def is_contains_chinese(strs):
-    for _char in strs:
-        if '\u4e00' <= _char <= '\u9fa5':
-            return True
-    return False
 
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()  # " ".join(page.extract_text().split())
-    return text
+def make_vectorstore(pdf_docs, collection_name):
+    text = get_pdf_text(pdf_docs)
+    text_chunks = get_text_chunks(text)
 
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n", chunk_size=1000, chunk_overlap=50, length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    print(len(chunks))
-    return chunks
-
-
-# def recreate_collection(client):
-#     vectors_config = VectorParams(
-#         size=768,
-#         distance=Distance.COSINE,
-#     )
-
-#     client.recreate_collection(
-#         collection_name=os.getenv("QDRANT_COLLECTION_NAME") or '',
-#         vectors_config=vectors_config,
-#     )
-
-
-def make_vectorstore(text_chunks, collection_name):
-    response = requests.get(
-        f'{os.getenv("QDRANT_HOST")}/collections/{os.getenv('QDRANT_COLLECTION_NAME')}',
-        timeout=3000,
-    )
-
-    print(response.json())
-
-    if response.status_code != 200 or not response.json()["result"]["vectors_count"]:
-        vector_store = doc_store.from_texts(
+    try:
+        Qdrant.from_texts(
+            url=os.getenv("QDRANT_URL"),
+            collection_name=collection_name,
             texts=text_chunks,
-            url="192.168.50.16:6333",
-            collection_name=collection_name or "test",
-            # api_key=os.getenv("QDRANT_API_KEY"),
             embedding=embeddings,
-            # force_recreate=True
+            prefer_grpc=True,
+            force_recreate=True,
         )
-        return vector_store
-    else:
-        return doc_store
+    except Exception as e:
+        print(e)
+
+    return
 
 
 def get_conversation_chain(vectorstore):
     llm = ChatGoogleGenerativeAI(
-        model="gemini-pro", convert_system_message_to_human=True
+        model="gemini-pro",
+        convert_system_message_to_human=True,
+        # client=db_client,
     )  # type: ignore
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
     # load vector store from qdrant
 
@@ -104,6 +58,28 @@ def get_conversation_chain(vectorstore):
         llm=llm, retriever=vectorstore.as_retriever(), memory=memory
     )
     return conversation_chain
+
+
+def get_db_collections() -> dict:
+    """get collections name and vector count"""
+    client = QdrantClient(os.getenv("QDRANT_URL"))
+
+    collections_list = [
+        collection["name"]
+        for collection in client.get_collections().model_dump()["collections"]
+    ]
+
+    info_list = [
+        client.get_collection(collection_name=collection).model_dump()["vectors_count"]
+        for collection in collections_list
+    ]
+
+    return {"collection_name": collections_list, "caption": info_list}
+
+
+def load_db_collection(collection):
+    print(collection)
+    return
 
 
 def handle_userinput(user_question):
@@ -120,19 +96,6 @@ def handle_userinput(user_question):
             st.write(
                 bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True
             )
-
-
-def get_db_collections() -> list:
-    return [
-        {"db_name": "11111", "caption": "11-11-1111"},
-        {"db_name": "2222", "caption": "22-2-2222"},
-        {"db_name": "33333", "caption": "3-3-3333"},
-    ]
-
-
-def load_db_collection(collection):
-    print(collection)
-    return doc_store
 
 
 def main():
@@ -153,8 +116,8 @@ def main():
             db_list = get_db_collections()
             db_selection = st.radio(
                 label="选择一个资料库",
-                options=[db["db_name"] for db in db_list],
-                captions=[db["caption"] for db in db_list],
+                options=db_list["collection_name"],
+                captions=[f"vectors:{n}" for n in db_list["caption"]],
                 horizontal=True,
             )
             print(db_selection)
@@ -182,14 +145,9 @@ def main():
             if collection_name and pdf_docs:
                 if st.button("处理"):
                     with st.spinner("处理中..."):
-                        # get pdf text
-                        raw_text = get_pdf_text(pdf_docs)
-
-                        # get the text chunks
-                        text_chunks = get_text_chunks(raw_text)
-
                         # create vector store
-                        make_vectorstore(text_chunks, collection_name)
+                        make_vectorstore(pdf_docs, collection_name)
+                        st.rerun()
 
     st.header(
         f"Chat with {st.session_state.collection if st.session_state.collection else 'your PDFs'} :books:"
---
+++ utils.py
@@ -0,0 +1,35 @@
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+def is_contains_chinese(strs):
+    for _char in strs:
+        if "\u4e00" <= _char <= "\u9fa5":
+            return True
+    return False
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += (
+                "".join(page.extract_text().split())
+                if is_contains_chinese(text)
+                else page.extract_text()
+            )
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="。" if is_contains_chinese(text) else "\n",
+        chunk_size=1000,
+        chunk_overlap=50,
+        length_function=len,
+    )
+    chunks = text_splitter.split_text(text)
+    print(len(chunks))
+    return chunks
+
